086434dafc90e14ed02349563466230e1fba4f26
Matt <smtrussell@gmail.com>
2020-04-03 13:39:01 -0500

    Updates

---
+++ precache-manifest.cd1b1185e1061c56727e92393d873268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf061a81ade422195ee0358e3d774ed",
+    "url": "/dice-roll-app/index.html"
+  },
+  {
+    "revision": "3ce9df82cf70c9d08041",
+    "url": "/dice-roll-app/static/css/main.96acc84e.chunk.css"
+  },
+  {
+    "revision": "b7e4ea081510a96778ed",
+    "url": "/dice-roll-app/static/js/2.461ed8ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roll-app/static/js/2.461ed8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ce9df82cf70c9d08041",
+    "url": "/dice-roll-app/static/js/main.13134a9d.chunk.js"
+  },
+  {
+    "revision": "30b16e59b3e3b9a043a1",
+    "url": "/dice-roll-app/static/js/runtime-main.c083b691.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dice-roll-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
