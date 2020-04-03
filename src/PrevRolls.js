389847a108a28cb501b9680057e2a187da687600
Matt <smtrussell@gmail.com>
2020-04-03 15:45:32 -0500

    previous rolls

--- src/PrevRolls.js
+++ src/PrevRolls.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import './PrevRolls.css'
 
 class PrevRolls extends Component {
-  render() {
-    const reversedRolls = this.props.rolls.reverse();
-    const previousRolls = reversedRolls.map((roll, index) => (
+  constructor(props) {
+    super(props);
+    this.displayRolls = this.displayRolls.bind(this);
+  }
+  
+  // reversedRolls = this.props.rolls.reverse();
+
+  reverseRolls() {
+    return this.props.rolls.reverse();
+  }
+
+  displayRolls() {
+    return this.reverseRolls().map((roll, index) => (
       <p>
         Roll {this.props.rolls.length - index} <i className={`fas fa-dice-${roll.first}`} ></i> <i className={`fas fa-dice-${roll.second}`} ></i>
       </p>
     ));
-
+  }
+  
+  render() {
     return(
       <div className='PrevRolls'>
         <div className='PrevRolls-list'>
-          {previousRolls}
+          {this.displayRolls()}
         </div>
       </div>
     );
